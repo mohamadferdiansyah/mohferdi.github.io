43ee4bd8bc7d0e5735feb069eddf78fcf66cd16f
mohamadferdiansyah <personalmohferdi@gmail.com>
2023-11-25 22:27:58 +0700

    responsive

--- jees.js
+++ jees.js
@@ -1,33 +1,46 @@
-document.addEventListener("scroll", function () {
-  var navbar = document.getElementById("navbar");
-  var scrolled = window.scrollY;
+// document.addEventListener("scroll", function () {
+//   var navbar = document.getElementById("navbar");
+//   var scrolled = window.scrollY;
 
-  if (scrolled > 200) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
+//   if (scrolled > 200) {
+//     navbar.classList.add("scrolled");
+//   } else {
+//     navbar.classList.remove("scrolled");
+//   }
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.getElementById("typeabout");
+    const text = "ABOUT ME.";
+    let index = 0;
+
+    function typeWriter() {
+        textElement.textContent += text[index];
+        index++;
+
+        if (index < text.length) {
+            setTimeout(typeWriter, 200); // Adjust the typing speed (milliseconds)
+        }
+    }
+
+    function startTyping() {
+        // Memeriksa jika scrollY mencapai atau melebihi 200
+        if (window.scrollY >= 50) {
+            // Hanya mulai mengetik jika belum mulai sebelumnya
+            if (index === 0) {
+                typeWriter();
+            }
+        } else {
+            // Reset index ke nilai awal jika scrollY di bawah 200
+            index = 0;
+            textElement.textContent = ""; // Menghapus teks yang sudah ditampilkan
+        }
+    }
+
+    // Menjalankan fungsi ketika halaman di-scroll
+    window.addEventListener("scroll", startTyping);
 });
 
-document.addEventListener("scroll", function () {
-  var typewriter = document.getElementById("typeabout");
-  var about = window.scrollY;
-
-  if (about > 80) {
-    typewriter.classList.add("about");
-  } else {
-    typewriter.classList.remove("about");
-  }
+document.getElementById("mobile-menu").addEventListener("click", function () {
+    this.classList.toggle("active");
 });
-
-
-document.addEventListener("scroll", function(){
-  var typewriter = document.getElementById("typeporto");
-  var titleporto = window.scrollY;
-
-  if (titleporto > 1300) {
-    typewriter.classList.add("titleporto");
-  } else {
-    typewriter.classList.remove("titleporto")
-  }
-})
